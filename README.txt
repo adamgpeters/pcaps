8a3c4b3fba0d885bf1d76310f8a3ec039948c125
Adam Peters <petersadam18@gmail.com>
2021-03-09 22:31:11 -0500

    finished

---
+++ README.txt
@@ -0,0 +1,24 @@
+Author: Adam Peters
+Date: 3/9/21
+
+Implementation: I believe that every aspect of this asignment have been 
+                completed correctly. 
+
+Collaborator: I discussed some of the different scans with Arundeep Singh
+
+Time spent: 10 hours
+
+Dependencies: base64
+
+Is the incident detection good?
+    All the detection tecniques I used seem extraordinarily arbitrary. 
+    I recognize that these scans (such as nikto) have practices that they 
+    keep to, and we can pick up on those different patterns, but nothing is 
+    changing the scanners from changing those scans. It's almost as if someone
+    is sneaking into your house and they always leave the TV on. You realize
+    this, and know when they come into your house... that is until they 
+    decide to leave the TV off.
+
+What would I add? 
+    If I had more time on this lab, I would try to research these scans more
+    and make the scans more robust (rather than arbitrary). 
--- alarm.py
+++ alarm.py
@@ -3,99 +3,135 @@
 from scapy.all import *
 import argparse
 import base64
-# print(base64.b64decode("YnJvZGdlcnM6VGhleVBsYXllZFdpdGhHcmVhdENoYXJhY3Rlcg=="))
-def ip_proto(pkt):
-    proto_field = pkt.get_field('proto')
-    return proto_field.i2s[pkt.proto]
-
-# x = IP() / TCP()
-# print(ip_proto(x))
 
 flags = {"FIN": 0x01, "SYN": 0x02, "RST": 0x04, "PSH": 0x08, "URG": 0x20}
+usernames = []
+passwords = []
 incidents_detected = 0
-# usernameIndicators = {F}
-TCP_REVERSE = dict((k, TCP_SERVICES[k]) for k in TCP_SERVICES.keys())
+
+def ip_proto(pkt):
+    """ returns the protcol used by a packet
+
+    Args:
+        pkt (Scapy Packet): packet from which the protocol is pulled
+
+    Returns:
+        String: the protocol used by the packet
+    """
+    proto_field = pkt[IP].get_field('proto')
+    return proto_field.i2s[pkt[IP].proto]
+
+def print_scan_incident(packet, incident, incidents_detected):
+    """ Prints an alert corresponding with the detected incident from the packet
+
+    Args:
+        packet (Scapy Packet): packet from which the incident was detected
+        incident (String): the scan detected as a string
+        incidents_detected (int): number of incidents detected
+    """
+    src_ip = packet[IP].src
+    protocol = ip_proto(packet)
+    print('ALERT #{}: {} is detected from {} ({})!'.format(          \
+                                                incidents_detected,  \
+                                                incident, src_ip,    \
+                                                protocol))
+
+
 def packetcallback(packet):
+    """ Sniffs for various incidents in packet
+
+    Args:
+        packet (Scapy Packet): the packet to be sniffed
+    """
     check_NULL_scan(packet)
     check_xmas_scan(packet)
     check_fin_scan(packet)
     check_nikto_scan(packet)
-    # check_smb_scan(packet)
+    check_smb_scan(packet)
     check_credentials_ftp(packet)
     check_credentials_imap(packet)
     check_credentials_HTTP(packet)
 
 def check_NULL_scan(packet):
+    """ Detects NULL scans in a packet
+
+    Args:
+        packet (Scapy Packet): the packet to be sniffed
+    """
     global incidents_detected
     try:    
         if packet[TCP].flags == 0:
             incidents_detected += 1
-            src_ip = packet[IP].src
-            protocol = ip_proto(packet[IP])
-            print('ALERT #{}: {} is detected from {} ({})!'.format( \
-                                                        incidents_detected,  \
-                                                        "NULL scan", src_ip, \
-                                                        protocol))
+            print_scan_incident(packet, "NULL scan", incidents_detected)
     except: 
         pass
 
-#FIN, PSH, and URG flags
 def check_fin_scan(packet):
+    """ Detects FIN scans in a packet
+
+    Args:
+        packet (Scapy Packet): the packet to be sniffed
+    """
     global incidents_detected   
     try:    
         if packet[TCP].flags == flags["FIN"]:
             incidents_detected += 1
-            src_ip = packet[IP].src
-            protocol = ip_proto(packet[IP])
-            print('ALERT #{}: {} is detected from {} ({})!'.format( \
-                                                        incidents_detected,  \
-                                                        "FIN scan", src_ip, \
-                                                        protocol))
+            print_scan_incident(packet, "FIN scan", incidents_detected)
     except: 
         pass
 
 def check_xmas_scan(packet):
+    """ Detects Xmas scans in a packet
+
+    Args:
+        packet (Scapy Packet): the packet to be sniffed
+    """
     global incidents_detected   
     try:    
         if packet[TCP].flags == (flags["FIN"] + flags["PSH"] + flags["URG"]):
             incidents_detected += 1
-            src_ip = packet[IP].src
-            protocol = ip_proto(packet[IP])
-            print('ALERT #{}: {} is detected from {} ({})!'.format( \
-                                                        incidents_detected,  \
-                                                        "Xmas scan", src_ip, \
-                                                        protocol))
+            print_scan_incident(packet, "Xmas scan", incidents_detected)
+
     except: 
         pass
 
 def check_nikto_scan(packet):  
+    """ Detects Nikto scans in a packet
+
+    Args:
+        packet (Scapy Packet): the packet to be sniffed
+    """
     global incidents_detected 
     try:    
         loadData = str(packet[TCP].load)
         if "Nikto" in loadData or "nikto" in loadData or "NIKTO" in loadData:
             incidents_detected += 1
-            src_ip = packet[IP].src
-            protocol = ip_proto(packet[IP])
-            print('ALERT #{}: {} is detected from {} ({})!'.format( \
-                                                        incidents_detected,  \
-                                                        "NIKTO scan", src_ip, \
-                                                        protocol))
+            print_scan_incident(packet, "NIKTO scan", incidents_detected)
+
     except: 
         pass
 
-x = 0
 def check_smb_scan(packet):   
-    global x
-    x += 1
-    if x < 100: 
-        try:    
-            print(ls(packet))
-        except: 
-            pass
-usernames = []
-passwords = []
-x = 0
+    """ Detects SMB scans in a packet
+
+    Args:
+        packet (Scapy Packet): the packet to be sniffed
+    """
+    global incidents_detected
+    try:    
+        data = str(raw(packet))
+        if "SMB" in data:
+            incidents_detected += 1
+            print_scan_incident(packet, "SMB scan", incidents_detected)
+    except: 
+        pass
+
 def check_credentials_ftp(packet):  
+    """ Detects credentials sent via FTP
+
+    Args:
+        packet (Scapy Packet): the packet to be sniffed
+    """
     global incidents_detected  
     try: 
         data = str(raw(packet))
@@ -116,6 +152,11 @@ def check_credentials_ftp(packet):
         pass
 
 def check_credentials_imap(packet):  
+    """ Detects credentials sent via imap
+
+    Args:
+        packet (Scapy Packet): the packet to be sniffed
+    """
     global incidents_detected 
     try: 
         if packet[TCP].sport == 143 or packet[TCP].dport == 143:
@@ -134,7 +175,13 @@ def check_credentials_imap(packet):
     except:
         pass
 
-def check_credentials_HTTP(packet):   
+def check_credentials_HTTP(packet):  
+    """ Detects credentials sent via HTTP
+
+    Args:
+        packet (Scapy Packet): the packet to be sniffed
+    """
+
     global incidents_detected
     try: 
         if packet[TCP].sport == 80 or packet[TCP].dport == 80:
@@ -142,11 +189,11 @@ def check_credentials_HTTP(packet):
             if "Authorization: Basic " in data:
                 incidents_detected += 1
                 parsedData = data.split("Authorization: Basic ")[1].split("\\")
-                pair = str(base64.b64decode(parsedData[0]))
-                pair = pair[2:len(pair)-1]
-                u_pass = pair.split(":")
-                usernames.append(u_pass[0])
-                passwords.append(u_pass[1])
+                u_pass_pair = str(base64.b64decode(parsedData[0]))
+                u_pass_pair = u_pass_pair[2:len(u_pass_pair)-1]
+                parsed_pair = u_pass_pair.split(":")
+                usernames.append(parsed_pair[0])
+                passwords.append(parsed_pair[1])
                 src_ip = packet[IP].src
                 payload = "username:{}, password:{}".format(usernames[-1], passwords[-1])
                 print('ALERT #{}: Usernames and passwords sent in-the-clear ({}) from {} ({})!'.format( \
@@ -155,8 +202,6 @@ def check_credentials_HTTP(packet):
                                             src_ip, payload))
     except:
         pass
-
-
 
 parser = argparse.ArgumentParser(description='A network sniffer that identifies basic vulnerabilities')
 parser.add_argument('-i', dest='interface', help='Network interface to sniff on', default='eth0')
